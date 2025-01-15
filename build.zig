ec2a9aacb26559e37d5ebb8a797c6b47a4a07566
Loris Cro <kappaloris@gmail.com>
2025-01-15 22:52:44 +0100

    end of day1

---
+++ build.zig
@@ -0,0 +1,40 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const exe = b.addExecutable(.{
+        .name = "kernel.elf",
+        .root_source_file = b.path("src/kernel.zig"),
+        .target = b.resolveTargetQuery(.{
+            .cpu_arch = .riscv32,
+            .os_tag = .freestanding,
+            .abi = .none,
+        }),
+        .optimize = .ReleaseSmall,
+        .strip = false,
+    });
+
+    exe.entry = .disabled;
+
+    exe.setLinkerScript(b.path("src/kernel.ld"));
+
+    b.installArtifact(exe);
+
+    // run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_cmd = b.addSystemCommand(&.{
+        "qemu-system-riscv32",
+    });
+
+    run_cmd.addArgs(&.{
+        "-machine",    "virt",
+        "-bios",       "default",
+        "-serial",     "mon:stdio",
+        "--no-reboot", "-nographic",
+        "-kernel",
+    });
+
+    run_cmd.addArtifactArg(exe);
+
+    const run_step = b.step("run", "Run QEMU");
+    run_step.dependOn(&run_cmd.step);
+}
